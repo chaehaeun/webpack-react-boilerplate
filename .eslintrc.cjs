c847fcb28e019097eb1c338089510de25a9c43b7
chaehaeun <chae_3210@naver.com>
2023-11-02 23:47:58 +0900

    [💡] 세팅 설명 주석 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,24 @@
 module.exports = {
+  // 이 설정이 프로젝트의 최상위에 위치하고 있음을 명시
   root: true,
+
+  // 코드 실행 환경을 정의
   env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:jsx-a11y/recommended',
-    'prettier',
+    'eslint:recommended', // ESLint의 권장 설정
+    'plugin:@typescript-eslint/recommended', // TypeScript 권장 설정
+    'plugin:react-hooks/recommended', // React Hooks 권장 설정
+    'plugin:import/recommended', // import 관련 권장 설정
+    'plugin:import/typescript', // TypeScript를 위한 import 설정
+    'plugin:jsx-a11y/recommended', // 접근성 (a11y) 관련 권장 설정
+    'prettier', // Prettier와 충돌하지 않는 ESLint 규칙을 사용
   ],
+
+  // lint 검사에서 제외할 패턴
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  // TypeScript를 위한 파서 사용
   parser: '@typescript-eslint/parser',
+  // 사용하는 ESLint 플러그인 목록
   plugins: ['react-refresh', 'jsx-a11y', 'react-hooks', 'import'],
   rules: {
     'import/order': [
@@ -39,6 +46,7 @@ module.exports = {
         pathGroupsExcludedImportTypes: ['react', 'unknown'],
         'newlines-between': 'always-and-inside-groups',
         alphabetize: {
+          // 알파벳 순서로 정렬 설정
           order: 'asc',
           caseInsensitive: true,
         },
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,95 @@
-const path = require("path");
+// Node.js 내장 모듈 'path'
+const path = require('path');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// HTML 파일을 생성하고 번들링된 자바스크립트를 주입해주는 플러그인
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  // 개발 모드 설정 (개발 모드로 설정하면 번들링된 파일이 가독성이 좋게 출력됨)
+  mode: 'development',
+  // 엔트리 포인트를 설정. 웹팩이 번들을 시작할 시작점
   entry: {
-    main: "./src/main.tsx",
+    main: './src/main.tsx',
   },
+  // 번들링된 결과물을 반환하는 설정
   output: {
-    path: path.resolve(__dirname, "dist"),
+    // 번들링된 파일의 이름
+    path: path.resolve(__dirname, 'dist'),
   },
+
+  // 모듈 해석 방법에 관한 설정
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    // 해당 확장자를 갖는 파일들은 웹팩이 번들링 할 때 모듈로 인식함
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+
+    // 모듈을 로드할 때, 해당 모듈을 찾기 위해 검색할 경로를 설정
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+
+    // 경로 별칭 설정. '@/components/MyComponent'와 같이 '@'로 시작하는 경로를 사용할 수 있음
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+
+  // 웹팩이 어떻게 다양한 유형의 파일을 처리해야하는지에 관한 규칙
   module: {
     rules: [
       {
+        // 해당 파일 확장자에 일치하는 모든 파일을 'file-loader'를 사용하여 처리함
         test: /\.(ttf|woff|woff2|eot)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
+        // TypeScript와 JavaScript 파일을 babel-loader를 사용하여 처리함
         test: /\.(tsx|ts|jsx|js)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
+        // 이미지 파일을 'file-loader'를 사용하여 처리함
         test: /\.(png|jpg|jpeg|gif)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[hash].[ext]",
-              outputPath: "images/",
+              // 출력 파일의 이름 설정
+              name: '[name].[hash].[ext]',
+              // 출력 파일의 경로 설정
+              outputPath: 'images/',
             },
           },
         ],
       },
       {
+        // SVG 파일 처리. '@svgr/webpack'은 SVG를 리액트 컴포넌트로 변환하고 'file-loader'는 파일로 처리함
         test: /\.svg$/,
-        use: ["@svgr/webpack", "file-loader"],
+        use: ['@svgr/webpack', 'file-loader'],
       },
       {
+        // CSS 파일을 처리. 'style-loader'는 스타일을 DOM에 삽입하고 'css-loader'는 CSS를 모듈로 해석함
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
+
+  // 개발 서버 설정
   devServer: {
+    // 압축하여 제공
     compress: true,
+    // 서버의 포트 번호
     port: 3000,
+    // HMR(Hot Module Replacement) 활성화
     hot: true,
+    // 단일 페이지 어플리케이션의 라우팅 지원
     historyApiFallback: true,
   },
+
+  // 웹팩 플러그인 설정
   plugins: [
+    // HtmlWebpackPlugin를 사용하여 'index.html' 템플릿을 기반으로 최종 HTML 파일을 생성함
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
   ],
 };
